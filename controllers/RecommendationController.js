0aff2ee19a2af032d1687e222a609cb40eaddc91
tsheasha <tarek.sheasha@gmail.com>
2014-06-21 23:03:10 +0200

    Adjusted JSON response of recommendations

--- controllers/RecommendationController.js
+++ controllers/RecommendationController.js
@@ -8,7 +8,7 @@ exports.recommend = function(request, response) {
     recommender.recommendation(request.query.user)
         .then(
             function success(data) { 
-                response.statusCode = 200; response.json(data); 
+                response.statusCode = 200; response.json({"list":data}); 
             },
             function failure(data) { 
                 response.statusCode = 500; response.send("failure"); 
